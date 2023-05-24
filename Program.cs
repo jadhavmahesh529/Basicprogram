3fa32b9730ff24903496401ada67bfa9cc6765ca
“Enter <jadhavmahesh529@gmail.com>
2023-05-24 15:35:20 +0530

    Add: difference of two number

--- Program.cs
+++ Program.cs
@@ -16,6 +16,16 @@ namespace Basic
             int sum = num1 + num2;
             Console.WriteLine("The sum of {0} and {1} is: {2}", num1, num2, sum);
 
+            Console.WriteLine("Enter the first number");
+            int no1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            int no2 = Convert.ToInt32(Console.ReadLine());
+
+            int difference = no1 - no2;
+            Console.WriteLine("The difference of {0} and {1} is: {2}", no1, no2, difference);
+
+
         }
     }
 }
